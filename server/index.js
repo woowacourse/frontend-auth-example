a97310566f354c850949c6c3f6f9df3410ddfeff
Cron <hojoong.choi@woowahan.com>
2023-05-18 14:50:22 +0900

    feat: 사용자 인증과 장바구니 결합

--- server/index.js
+++ server/index.js
@@ -10,6 +10,25 @@ const cors = require("cors");
  * ( o̴̶̷̥᷅⌓o̴̶̷᷄ ) ( o̴̶̷̥᷅⌓o̴̶̷᷄ ) ( o̴̶̷̥᷅⌓o̴̶̷᷄ )
  */
 
+const userCarts = {
+  "user@example.com": [
+    {
+      id: 5,
+      name: "[버거킹] 치킨너겟 100봉지",
+      price: 200000,
+      imageUrl:
+        "https://images.unsplash.com/photo-1582981760753-b52aae38f237?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+    },
+    {
+      id: 4,
+      name: "[간편식] 간단한 다진고기 볶음과 웨지감자",
+      price: 6000,
+      imageUrl:
+        "https://images.unsplash.com/photo-1518510227856-30619d542ea9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+    },
+  ],
+};
+
 app.use(cors());
 app.use(
   basicAuth({
@@ -20,6 +39,17 @@ app.use(
 
 app.get("/", (req, res) => {
   res.send("You are authenticated!");
+});
+
+app.get("/cart-items", (req, res) => {
+  const user = req.auth.user;
+  const cart = userCarts[user];
+
+  if (!cart) {
+    res.status(404).send("Not found");
+    return;
+  }
+  res.send(cart);
 });
 
 app.listen(3000, () => {
